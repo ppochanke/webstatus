c24d69249b93ed7895a75660fdc868337ccaa457
Piotr Pochanke <ppochanke@gmail.com>
2019-11-16 17:49:14 +0100

    Init commit

--- test/test.py
+++ test/test.py
@@ -3,27 +3,41 @@ import webstatus
 
 
 class MyTestCase(unittest.TestCase):
+    def __prepare_no_redirect_url_list(self):
+        urls = ["http://www.google.com", "http://www.google.pl", "https://www.onet.pl", "https://www.wp.pl",
+                "https://jenkins.io", "https://stackoverflow.com"]
+        return urls
 
-    def test_url_status_correct_url_no_follow(self):
-        url = "http://www.google.com"
+    def __prepare_redirect_url_list(self):
+        urls = ["http://google.com", "http://google.pl", "http://onet.pl", "http://www.wp.pl", "http://jenkins.io",
+                "http://stackoverflow.com"]
+        return urls
+
+    def test_url_status_no_redirect_url_no_follow_status_eq_200(self):
+        urls = self.__prepare_no_redirect_url_list()
         follow = False
-        result = webstatus.Main().url_status(url, follow)
-        self.assertEqual(200, result)
 
-    def test_url_status_incorrect_url_no_follow(self):
-        url = "http://google.com"
-        follow = False
-        result = webstatus.Main().url_status(url, follow)
-        self.assertNotEqual(200, result)
+        for url in urls:
+            result = webstatus.Main().url_status(url, follow)
+            self.assertEqual(200, result)
 
-    def test_url_status_correct_url_follow(self):
-        url = "http://google.com"
+    def test_url_status_redirect_url_follow_status_eq_200(self):
+        urls = self.__prepare_redirect_url_list()
         follow = True
-        result = webstatus.Main().url_status(url, follow)
-        self.assertEqual(200, result)
+
+        for url in urls:
+            result = webstatus.Main().url_status(url, follow)
+            self.assertEqual(200, result)
+
+    def test_url_status_redirect_url_no_follow_status_eq_301(self):
+        urls = self.__prepare_redirect_url_list()
+        follow = False
+
+        for url in urls:
+            result = webstatus.Main().url_status(url, follow)
+            self.assertEqual(301, result)
 
 
 if __name__ == '__main__':
     unittest.main()
-
 
